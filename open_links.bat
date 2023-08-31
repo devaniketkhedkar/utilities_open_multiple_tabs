00ff16c4f6bbf4827d88b6045ec0692499fe60a7
Aniket Khedkar <102348408+devaniketkhedkar@users.noreply.github.com>
2023-08-31 15:30:45 +0530

    Version 1.0

---
+++ open_links.bat
@@ -0,0 +1,8 @@
+@echo off
+start https://tanla.freshdesk.com/a/tickets/filters/377650
+start http://10.60.120.46:3000/d/r1q1G9qGk/jobs-status-stats?orgId=1&refresh=5m
+start http://10.60.60.46:3000/d/6fBFjzSMz/bsnl-jobs-status?orgId=1
+start http://10.10.6.29:3000/d/BYvnL_yWz/jobs-status?orgId=1&refresh=5m
+start http://10.201.182.8:3000/d/boqjhhknz/vih-stats-db-jobs?orgId=1
+start http://10.213.80.118:3000/d/gIe9uB37k/job-status?orgId=1&refresh=5m
+start https://solarwinds.tanla.com/orion/netperfmon/alerts.aspx
