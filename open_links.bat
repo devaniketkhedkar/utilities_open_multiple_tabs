6fd4f8b56464bacd23cea75f0816660afeaa981b
Aniket Khedkar <102348408+devaniketkhedkar@users.noreply.github.com>
2024-07-18 14:51:06 +0530

    Update open_links.bat

--- open_links.bat
+++ open_links.bat
@@ -1,8 +1,7 @@
 @echo off
-start https://tanla.freshdesk.com/a/tickets/filters/377650
-start http://10.60.120.46:3000/d/r1q1G9qGk/jobs-status-stats?orgId=1&refresh=5m
-start http://10.60.60.46:3000/d/6fBFjzSMz/bsnl-jobs-status?orgId=1
-start http://10.10.6.29:3000/d/BYvnL_yWz/jobs-status?orgId=1&refresh=5m
-start http://10.201.182.8:3000/d/boqjhhknz/vih-stats-db-jobs?orgId=1
-start http://10.213.80.118:3000/d/gIe9uB37k/job-status?orgId=1&refresh=5m
-start https://solarwinds.tanla.com/orion/netperfmon/alerts.aspx
+start https://mock.bseindia.com/
+start https://www.w3schools.com/
+start https://www.w3schools.com/html/default.asp
+start https://www.w3schools.com/css/default.asp
+start https://www.w3schools.com/sql/default.asp
+start https://www.w3schools.com/java/default.asp
